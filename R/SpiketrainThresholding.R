9e389878ae4d9a0dd75b657dc61cdd775ebcff45
Vasileios <sideridisvasileios@gmail.com>
2020-11-16 13:16:02 +0200

    the SpiketrainThresholding function ws added

---
+++ R/SpiketrainThresholding.R
@@ -0,0 +1,35 @@
+SpiketrainThresholding = function(deconvolvedSignal, fluorescense, minNonZeroS_noise = 5, threshold_dc, isHardThreshold = FALSE){
+
+  # Simple hard threshold in deconvolved signal
+  if(isHardThreshold){
+    threshold_deconvolved = 0.01
+    spiketrain = ifelse(deconvolvedSignal > threshold_deconvolved, 1,0)
+    return(spiketrain)
+  }
+
+  totalFrames = length(deconvolvedSignal)
+
+  # Noise intervals are the frames where fluorescense is negative
+  noise_intervals = fluorescense<0
+  # Make a circular shift to Noise intervals of 1 frame to the left
+  noise_intervals = c(noise_intervals[2:length(noise_intervals)], FALSE)
+
+  # s_noise is the deconvolvedSignal restricted on the noise intervals
+  s_noise = deconvolvedSignal[noise_intervals]
+
+  # if the number of nonzero s_noise values are less than minNonZeroS_noise,
+  # then the std is calculated from the deconvolved signal s itself.
+  numOfNonzeroValues =  sum(s_noise>0.01)
+
+  if(numOfNonzeroValues <= minNonZeroS_noise){
+    stdValue = quantile(deconvolvedSignal[which(deconvolvedSignal>0.0001)], .68)
+  }else{
+    stdValue = quantile(s_noise[which(s_noise>0.0001)], .68)
+  }
+
+  #Threshold is calculated by the product of noise's std and the threshold_dc which is given by the user
+  threshold_deconvolved = as.numeric(threshold_dc*stdValue)
+
+  spiketrain = ifelse(deconvolvedSignal > threshold_deconvolved, 1,0)
+  return(spiketrain)
+}
